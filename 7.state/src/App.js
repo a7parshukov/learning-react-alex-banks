6476f43a5edffb42d7e7b708d97d670dacc9e3ef
a7parshukov <a7parshukov@gmail.com>
2023-06-22 15:26:58 +0300

    state star

---
+++ 7.state/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import StarRating from './components/StarRating';
+
+function App() {
+  return (
+    <>
+      <h1>1.2.3</h1>
+      <StarRating totalStars={5} />
+    </>
+  );
+}
+
+export default App;
