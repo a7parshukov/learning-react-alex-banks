9faa0c919ade4fc64ab85a119ea5bbfe623b9ec2
a7parshukov <a7parshukov@gmail.com>
2023-06-23 13:47:33 +0300

    state

--- 7.state/src/components/StarRating.jsx
+++ 7.state/src/components/StarRating.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
-const Star = ({ selected = false }) => (
-	<FaStar color={selected ? "red" : "grey"} />
+const hello = () => console.log("hello")
+
+const Star = ({ selected = false, onSelect = func => func }) => (
+	<FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
 )
 
 const createArray = length => [...Array(length)];
 
-export default function StarRating({ totalStars = 5 }) {
+export default function StarRating({ style = {}, totalStars = 5 }) {
+	const [selectedStars, setSelectedStars] = useState(1);
+
 	return (
-		createArray(totalStars).map((n, i) => <Star key={i}/>)
+		<div style={{ padding: "5px", ...style }}>
+			{createArray(totalStars).map((n, i) => (
+				<Star
+					key={i}
+					selected={selectedStars > i}
+					onSelect={() => setSelectedStars(i + 1)}
+				/>
+			))}
+			<p>
+				{selectedStars} of {totalStars} stars
+			</p>
+		</div>
 	)
 }
